9b6548b746229e879014ff88e25b64e625c27ee5
krzysztof.kolek.praca <krzysztof.kolek.praca@gmail.com>
2016-06-07 16:07:59 +0200

    BaseRepository, RepositoryFactory - fix: added missing file

---
+++ Repository/Main/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventureWorks.Repository.Main
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using NHibernate;
+    using NHibernate.Linq; 
+
+    public abstract class BaseRepository<T>
+    {
+        public virtual System.Collections.Generic.IList<T> BaseSearchFor(System.Linq.Expressions.Expression<System.Func<T, bool>> predicate)
+        {
+            using (ISession session = SessionManager.OpenSession())
+            {
+                return session.Query<T>().Where(predicate).ToList();
+            }
+        }
+
+        public virtual System.Collections.Generic.IList<T> BaseGetAll()
+        {
+            using (ISession session = SessionManager.OpenSession())
+            {
+                return session.Query<T>().AsQueryable().ToList();
+            }
+        }
+
+        public virtual T BaseGetById(int id)
+        {
+            using (ISession session = SessionManager.OpenSession())
+            {
+                return session.Get<T>(id);
+            }
+        }
+
+        public virtual void BaseInsert(T entity)
+        {
+            using (ISession session = SessionManager.OpenSession())
+            {
+                session.SaveOrUpdate(entity);
+            }
+        }
+
+        public virtual void BaseDelete(T entity)
+        {
+            using (ISession session = SessionManager.OpenSession())
+            {
+                session.Delete(entity);
+            }
+        }
+    }
+}
---
+++ Repository/Main/RepositoryFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventureWorks.Repository.Main
+{
+    using Castle.Windsor;
+    using Castle.Windsor.Installer;
+
+    public class RepositoryFactory<T>
+        where T: class, new()
+    {
+        private IWindsorContainer _container;
+
+        public RepositoryFactory()
+        {
+            _container = new WindsorContainer();
+            _container.Install(FromAssembly.This());
+        }
+
+        public IRepository<T> GetRepository()
+        {
+            return _container.Resolve<IRepository<T>>();
+        }
+
+        public IEditableRepository<T> GetEditableRepository()
+        {
+            return _container.Resolve<IEditableRepository<T>>();
+        }
+        public IReadWriteRepository<T> GetReadWriteRepository()
+        {
+            return _container.Resolve<IReadWriteRepository<T>>();
+        }
+    }
+}
