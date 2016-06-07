b02ed3b59455de100665f20fba943fa9741bb9b0
krzysztof.kolek.praca <krzysztof.kolek.praca@gmail.com>
2016-06-07 19:17:08 +0200

    Added a single Console application to test the container Added a repository test project UOW simple test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    using AdventureWorks.UOW;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 test = new Class1();
+            test.GetTest();
+        }
+    }
+}
