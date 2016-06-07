6a3f3be7b8bd12dc4129ac92b4f1751ab5a76738
krzysztof.kolek.praca <krzysztof.kolek.praca@gmail.com>
2016-06-07 19:37:10 +0200

    Issue - Missing file Fix - added UOW\Class1.cs

---
+++ UOW/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.UOW
+{
+    using Castle.Core;
+    using EntityClasses.Production;
+    using Repository.DI;
+    using Repository.Main;
+
+    [Interceptor(typeof(CustomInterceptor))]
+    public class Class1
+    {
+        private IRepository<Product> _rep;
+
+        //public Class1(IRepository<Product> repository)
+        //{
+        //    _rep = repository;
+        //}
+
+        public void GetTest()
+        {
+            var test = new RepositoryFactory<Product>().GetRepository().GetAll();
+            var test1 = new RepositoryFactory<Product>().GetRepository().GetAll();
+            var test2 = new RepositoryFactory<Product>().GetRepository().GetAll();
+        }
+    }
+}
