5b3a5577fb5f7ddb1b37a57d335b64cf227f289c
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-07 13:29:11 +0200

    XML Serializer/Deserializer

---
+++ AdventureWorks.Utils/XMLManager.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventureWorks.Utils
+{
+    using System;
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Serialization;
+
+    public class XMLManager<T>
+        where T : class, new()
+    {
+        private XmlSerializer _serializer { get; set; }
+
+        public XMLManager()
+        {
+            _serializer = new XmlSerializer(typeof(T));
+        }
+
+        public void Serialize(T input, String path)
+        {
+            using (var writer = new FileStream(path, FileMode.Create))
+            {
+                _serializer.Serialize(writer, input);
+            }
+        }
+
+        public T Deserialize(String path)
+        {
+            T type;
+            using (var reader = XmlReader.Create(path))
+            {
+                type = _serializer.Deserialize(reader) as T;
+            }
+            return type;
+        }
+    }
+}
