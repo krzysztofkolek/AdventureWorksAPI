f30f24d042fd75c3d2f90702b6cc7105089b8c21
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-09 19:36:00 +0200

    AdventureWorks.Utils Added PropertiesFiller.cs - responsible for data serialization from NHibernate.

---
+++ AdventureWorks.Utils/PropertiesFiller.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventureWorks.Utils
+{
+
+    public class PropertiesFiller
+    {
+        public dynamic CreateNewObject(dynamic oldObject, dynamic newobject)
+        {
+            var props = oldObject.GetType().GetProperties();
+            var declaratedTypesNewObject = ((System.Reflection.FieldInfo[])(((System.Reflection.TypeInfo)(newobject.GetType())).DeclaredFields));
+
+            foreach (var item in declaratedTypesNewObject)
+            {
+                for (int i = 0; i < props.Length; i++)
+                {
+                    if (props[i].Name.Equals(item.Name))
+                    {
+                        var value = props[i].GetValue(oldObject, null);
+                        item.SetValue(newobject, value);
+                    }
+                }
+            }
+            return newobject;
+        }
+    }
+}
