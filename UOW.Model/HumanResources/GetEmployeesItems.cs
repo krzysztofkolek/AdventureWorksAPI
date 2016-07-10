b9793a05dae6aa3acfe7ac3af10619ffb5083821
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-10 20:31:00 +0200

    Missing File Addd

---
+++ UOW.Model/HumanResources/GetEmployeesItems.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventureWorks.UOW.Model.HumanResources
+{
+    using System;
+    public class GetEmployeesItems
+    {
+        public int Id { get; set; }
+        public String FullName { get; set; }
+        public String Title { get; set; }
+        public String ManagerFullName { get; set; }
+        public String HireDate { get; set; }
+        public String Salary { get; set; }
+    }
+}
