31c83ebd4b6a61ffbb5008605f67ab99a834a098
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-11 09:30:44 +0200

    Added missing file

---
+++ Repository/Repository/HumanResources/EmployeeExtendedRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventureWorks.Repository.HumanResources
+{
+    using AdventureWorks.EntityClasses.HumanResources;
+    using AdventureWorks.Repository.Extension.Model.HumanResources;
+    using System.Collections.Generic;
+
+    public class EmployeeExtendedRepository<T> : EmployeeRepository<T>
+        where T : Employee, new()
+    {
+
+
+
+        public IList<EmployeesPaging> GetEmployeesPaging(int page, int entityCount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
