35206d37bd8e0ceec2661a288379c85c62240f91
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-08-03 22:10:15 +0200

    404 Added a 404 page

--- AdventureWorks.API/Bootstrapper.cs
+++ AdventureWorks.API/Bootstrapper.cs
@@ -10,7 +10,7 @@
         protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
         {
             TokenAuthentication.Enable(pipelines, new TokenAuthenticationConfiguration(container.Resolve<ITokenizer>()));
-            
+
             container.AutoRegister();
         }
     }
